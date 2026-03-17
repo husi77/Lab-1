8a81bf596e9fc6ac63cd21d915c7155ac5998696
husi77 <mhussain2kev@gmail.com>
2026-03-17 21:44:24 +0500

    Refactor binary search and result display functions

--- Lab 02/Task5/Taask5.cpp
+++ Lab 02/Task5/Taask5.cpp
@@ -1,59 +1,55 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 template <typename T, size_t N>
-int binarySearch(T(&A)[N], T Value) {
-    int low = 0;
-    int high = N - 1;
+int searchValue(T (&arr)[N], T key)
+{
+    int left = 0;
+    int right = N - 1;
 
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
 
-        if (A[mid] == Value) {
+        if (arr[mid] == key)
             return mid;
-        }
-        else if (A[mid] < Value) {
-            low = mid + 1;
-        }
-        else {
-            high = mid - 1;
-        }
+        else if (arr[mid] < key)
+            left = mid + 1;
+        else
+            right = mid - 1;
     }
 
     return -1;
 }
 
 template <typename T>
-void printSearchResult(int index, T key) {
-    if (index != -1) {
-        cout << key << " found at index " << index << "\n";
-    }
-    else {
-        cout << key << " not found\n";
-    }
+void showResult(int pos, T value)
+{
+    if (pos != -1)
+        cout << value << " found at index " << pos << endl;
+    else
+        cout << value << " not found" << endl;
 }
 
-int main() {
+int main()
+{
+    int nums[5] = {11, 12, 22, 25, 64};
+    int key1 = 22;
+    int pos1 = searchValue(nums, key1);
+    showResult(pos1, key1);
 
-    int intArray[5] = { 11, 12, 22, 25, 64 };
-    int intKey = 22;
+    float decimals[4] = {0.57, 1.62, 2.71, 3.14};
+    float key2 = 2.71;
+    int pos2 = searchValue(decimals, key2);
+    showResult(pos2, key2);
 
-    int intIndex = binarySearch(intArray, intKey);
-    printSearchResult(intIndex, intKey);
-
-    float floatArray[4] = { 0.57, 1.62, 2.71, 3.14 };
-    float floatKey = 2.71;
-
-    int floatIndex = binarySearch(floatArray, floatKey);
-    printSearchResult(floatIndex, floatKey);
-
-    string stringArray[4] = { "apple", "banana", "grape", "orange" };
-    string stringKey = "grape";
-
-    int stringIndex = binarySearch(stringArray, stringKey);
-    printSearchResult(stringIndex, stringKey);
+    string cars[4] = {"Audi", "BMW", "Honda", "Toyota"};
+    string key3 = "Honda";
+    int pos3 = searchValue(cars, key3);
+    showResult(pos3, key3);
 
     return 0;
 }
+
+
